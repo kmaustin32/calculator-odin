4fe62ca81296a521be57be919d47b716f452a5e5
Keenan (Ezra) Austin <kmaustin32@gmail.com>
2024-03-19 11:42:37 +0200

    Add checkZero standalone function

--- scripts.js
+++ scripts.js
@@ -14,6 +14,19 @@ const currentObj = () => {
   });
 };
 
+const checkZero = () => {
+  if (calcObj.operator == '/' && calcObj.current == '0') {
+    display.textContent = "Bad!";
+    setTimeout(() => {
+      display.textContent = calcObj.last;
+      calcObj.current = '';
+    }, 800);
+    currentObj();
+    return true;
+  };
+  return false;
+};
+
 const add = () => {
   if (calcObj.last === '') calcObj.last = 0;
   return parseFloat(calcObj.last) + parseFloat(calcObj.current);
@@ -38,7 +51,6 @@ const operate = () => {
   let result = 0;
 
   if (calcObj.current === '') return;
-  if (calcObj. current === '0' && calcObj.operator === '/') return "BAD...";
 
   switch (calcObj.operator) {
     case "+": {
@@ -65,15 +77,7 @@ const operate = () => {
 
 const calculateButton = document.querySelector('#calculate');
 calculateButton.addEventListener('click', (e) =>{
-  if (calcObj.operator == '/' && calcObj.current == '0') {
-    display.textContent = "Bad!";
-    setTimeout(() => {
-      display.textContent = calcObj.last;
-      calcObj.current = '';
-    }, 800);
-    currentObj();
-    return;
-  };
+  if(checkZero()) return;
   
   if (!calcObj.current || !calcObj.last) {
     currentObj();
@@ -148,10 +152,13 @@ document.addEventListener('keydown', (e) => {
   console.log(pressed);
 
   if(pressed === "=" || pressed === "Enter") {
+    if (checkZero()) return;
+
     if (!calcObj.current || !calcObj.last) {
       currentObj();
       return;
-    }
+    };
+
     let solution = operate(calcObj.operator);
     calcObj.last = solution;
     display.textContent = solution;
@@ -159,17 +166,20 @@ document.addEventListener('keydown', (e) => {
     calcObj.operator = '';
     currentObj();
   };
+
   if (pressed === "Backspace") {
     let newCurrent = calcObj.current.slice(0, -1);
     calcObj.current = newCurrent;
     display.textContent = calcObj.current;
   };
+
   if (pressed === 'Delete') {
     calcObj.current = '';
     calcObj.last = '';
     calcObj.operator = '';
     display.textContent = '';
   };
+
   if (pressed === '+' || pressed === '-' || pressed === '*' || pressed === '/') {
     if (!calcObj.current) {
       calcObj.current = calcObj.last;
